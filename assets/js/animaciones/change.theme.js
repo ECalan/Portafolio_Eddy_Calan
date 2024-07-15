296a907964e54bbbcc60c67d15f4562fe849fed2
ECalan <tecnoelectronictrance@gmail.com>
2024-07-14 18:20:54 -0600

    Se hicieron cambios en el cambio de tema

--- assets/js/animaciones/change.theme.js
+++ assets/js/animaciones/change.theme.js
@@ -1,13 +1,12 @@
 const themes = document.querySelector("#themes");
 
-themes.addEventListener("change", () =>{
-    const cambioThema = themes.value;
-    if(cambioThema == "Dark"){
-        document.querySelector("body").style ="background-color: var(--cor-de-fondo2);";
-    }else
-    if(cambioThema == "White"){
-        document.querySelector("body").style ="background-color: var(--cor-de-fondo1);";
-    }       
+themes.addEventListener("change", () => {
+  const cambioThema = themes.value;
+  if (cambioThema == "Dark") {
+    document.querySelector("body").style =
+      "background-color: var(--cor-de-fondo2);";
+  } else if (cambioThema == "White") {
+    document.querySelector("body").style =
+      "background-color: var(--cor-de-fondo1);";
+  }
 });
-
-
